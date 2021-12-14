376d35ebf42ec6f85eedb5564b23e4b8012d577b
Hijir Ismail <hijirismail007@gmail.com>
2021-12-14 18:48:33 +0800

    update SelectedKuliner

--- app/src/main/java/id/ac/unhas/winfood/ui/selected/SelectedKuliner.kt
+++ app/src/main/java/id/ac/unhas/winfood/ui/selected/SelectedKuliner.kt
@@ -1,4 +1,49 @@
 package id.ac.unhas.winfood.ui.selected
 
-class SelectedKuliner {
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import id.ac.unhas.winfood.R
+import id.ac.unhas.winfood.utilities.StaticData
+import kotlinx.android.synthetic.main.fragment_selected_kuliner.*
+
+class SelectedKuliner : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selected_kuliner, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        nama_selected.text = StaticData.nama
+        jam_selected.text = StaticData.jamBukaTutup
+        alamat_selected.text = StaticData.alamat
+        koordinat_selected.text = StaticData.kordinat
+        kategori_selected.text = StaticData.kategori
+
+        share.setOnClickListener{
+            val nama = StaticData.nama.toUpperCase()
+            val kategori = StaticData.kategori
+            val alamat = StaticData.alamat
+            val sendIntent: Intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Hai friend! Jika jalan-jalan ke Purwekerto, $nama adalah rekomendasi tempat kuliner yang harus kamu kunjungi! \n$kategori \n$alamat")
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = SelectedKuliner()
+    }
 }
