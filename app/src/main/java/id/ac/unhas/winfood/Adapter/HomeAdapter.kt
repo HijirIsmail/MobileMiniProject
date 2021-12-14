f930c8f8698e4eb25d7b8e56c1c4b3ce1687c571
Hijir Ismail <hijirismail007@gmail.com>
2021-12-14 18:18:33 +0800

    update and add api

--- app/src/main/java/id/ac/unhas/winfood/Adapter/HomeAdapter.kt
+++ app/src/main/java/id/ac/unhas/winfood/Adapter/HomeAdapter.kt
@@ -1,4 +1,42 @@
 package id.ac.unhas.winfood.Adapter
 
-class HomeAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import id.ac.unhas.winfood.KulinerItem
+import id.ac.unhas.winfood.R
+import kotlinx.android.synthetic.main.item_home.view.*
+
+class HomeAdapter(private val listMakanan: ArrayList<KulinerItem>, val listener: OnItemClickListener) : RecyclerView.Adapter<HomeAdapter.Homeholder>() {
+
+    interface OnItemClickListener{
+        fun onClick(kulinerItem: KulinerItem)
+    }
+
+    class Homeholder(itemview: View): RecyclerView.ViewHolder(itemview){
+        fun bind(kulinerItem: KulinerItem, listener: OnItemClickListener){
+            with(itemView){
+                nama.text = kulinerItem.nama
+                kategori.text=kulinerItem.kategori
+            }
+            itemView.setOnClickListener{
+                listener.onClick(kulinerItem)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.Homeholder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home, parent, false)
+        return Homeholder(view)
+
+    }
+
+    override fun getItemCount(): Int {
+        return listMakanan.size
+    }
+
+    override fun onBindViewHolder(holder: HomeAdapter.Homeholder, position: Int) {
+        holder.bind(listMakanan[position],listener)
+    }
 }
--- app/src/main/java/id/ac/unhas/winfood/DataKuliner.kt
+++ app/src/main/java/id/ac/unhas/winfood/DataKuliner.kt
@@ -1,2 +1,12 @@
 package id.ac.unhas.winfood
 
+data class KulinerItem(val jamBukaTutup: String,
+                       val kordinat: String,
+                       val nama: String,
+                       val gambarUrl: String,
+                       val kategori: String,
+                       val id: Int = 0,
+                       val alamat: String)
+
+
+data class DataKuliner(val kuliner: List<KulinerItem>?)
